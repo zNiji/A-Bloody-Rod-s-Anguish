7c7c0873dc3bdeffca9f2e6181f0de0891f86390
LiamFullard <liamfullard23@gmail.com>
2025-10-27 12:22:52 +0200

    Audio and Settings
    - Added background music
    - Added adjustable volume slider
    - Added Fullscreen toggle setting

---
+++ Assets/Audio/SoundManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("volume"))
+        {
+            PlayerPrefs.SetFloat("volume", 1f);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
+    }   
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,4 +13,9 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void setFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
 }
