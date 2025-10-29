40ee5f351357f88742e962a80be980b1def3d38a
LiamFullard <liamfullard23@gmail.com>
2025-10-29 09:40:04 +0200

    Added don't destroy on load
    This allows the player character to persist and resets its position through multiple scene loads and changes using singletons

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     float speedX, speedY;
-    Rigidbody2D rb; 
+    Rigidbody2D rb;
+    private static PlayerController instance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,5 +20,33 @@ public class PlayerController : MonoBehaviour
         speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
         speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
         rb.linearVelocity = new Vector2(speedX, speedY);
+    }
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Reset position or state when level restarts
+        transform.position = Vector3.zero;
+        // Optionally reset health, score, etc.
     }
 }
