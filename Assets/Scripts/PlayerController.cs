b698b69676b31cb6c345a2cea8d4207e289bf197
LiamFullard <liamfullard23@gmail.com>
2025-10-29 09:13:16 +0200

    Created Character Controller
    - Movement using WASD
    - Added character sprite

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
